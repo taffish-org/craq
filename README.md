95a4594bbda2719bc1a9584f0d51bd4c70a16c59
Kaiyuan Han <hermit_han@163.com>
2025-08-30 15:25:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# taf-craq
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/JiaoLaboratory/CRAQ
+
+Identification of errors in draft genome assemblies with single-base pair resolution for quality assessment and improvement
---
+++ craq.info.lisp
@@ -0,0 +1,11 @@
+(:name           "craq")
+(:repo           "taffish-org/craq")
+(:author         "KaiyuanHan")
+(:version        "1.0.9-alpha")
+(:intro          "Identification of errors in draft genome assemblies with single-base pair resolution for quality assessment and improvement")
+(:dependencies)
+(:functions      "python [3.13.2] (python-circos, requests)"
+                 "perl [5]"
+                 "craq"
+                 "samtools [1.21]"
+                 "minimap2 [2.29]")
